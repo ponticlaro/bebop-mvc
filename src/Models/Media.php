9b274c463a66872343ca21f3e6abf76511fe4397
Cristiano Baptista <cristianobaptista@designdesu.com>
2015-09-21 15:30:43 +0200

    added methods to only get attachment data on demand, instead of automatically loading it all via Bebop Core

--- src/Models/Media.php
+++ src/Models/Media.php
@@ -4,12 +4,25 @@ namespace Ponticlaro\Bebop\Mvc\Models;
 
 class Media extends Post {
 
-	/**
+    /**
      * Post type name
      * 
      * @var string
      */
     protected static $__type = 'attachment';
+
+    /**
+     * Contains ids of the existing image sizes
+     * 
+     * @var array
+     */
+    protected static $__sizes = [];
+
+    /**
+     * Contains data for each existing image size
+     * @var array
+     */
+    public $sizes = [];
 
     /**
      * Checks is a thumbnail with the target size id exists
@@ -20,26 +33,28 @@ class Media extends Post {
      */
     public function hasThumb($size, array $fallback_sizes = array())
     {
-    	$has_thumb = false;
+        static::__cacheExistingImageSizes();
 
-    	if (isset($this->sizes[$size])) {
-    		
-    		$has_thumb = true;
-    	}
+        $has_thumb = false;
 
-    	if (!$has_thumb && $fallback_sizes) {
-    		
-    		foreach ($fallback_sizes as $size) {
-    			
-    			if (is_string($size) && isset($this->sizes[$size])) {
-    				
-    				$has_thumb = true;
-    				break;
-    			}
-    		}
-    	}
+        if (in_array($size, static::$__sizes)) {
+            
+            $has_thumb = true;
+        }
 
-    	return $has_thumb;
+        if (!$has_thumb && $fallback_sizes) {
+            
+            foreach ($fallback_sizes as $size) {
+                
+                if (is_string($size) && in_array($size, static::$__sizes)) {
+                    
+                    $has_thumb = true;
+                    break;
+                }
+            }
+        }
+
+        return $has_thumb;
     }
 
     /**
@@ -51,28 +66,30 @@ class Media extends Post {
      */
     public function getThumb($size, array $fallback_sizes = array())
     {
-    	if (!is_string($size)) return null;
-    	
+        if (!is_string($size)) return null;
+        
+        static::__cacheExistingImageSizes();
+
         $thumb = null;
 
-    	if ($this->hasThumb($size)) {
-    		
-    		$thumb = $this->sizes[$size];
-    	}
+        if ($this->hasThumb($size)) {
+            
+            $thumb = $this->getThumbInfo($size);
+        }
 
-    	if (!$thumb && $fallback_sizes) {
-    		
-    		foreach ($fallback_sizes as $size) {
-    			
-    			if (is_string($size) && $this->hasThumb($size)) {
-    				
-    				$thumb = $this->sizes[$size];
-    				break;
-    			}
-    		}
-    	}
+        if (!$thumb && $fallback_sizes) {
+            
+            foreach ($fallback_sizes as $size) {
+                
+                if (is_string($size) && $this->hasThumb($size)) {
 
-    	return $thumb;
+                    $thumb = $this->getThumbInfo($size);
+                    break;
+                }
+            }
+        }
+
+        return $thumb;
     }
 
     /**
@@ -84,8 +101,63 @@ class Media extends Post {
      */
     public function getThumbUrl($size, array $fallback_sizes = array())
     {
-    	$thumb = $this->getThumb($size, $fallback_sizes);
+        static::__cacheExistingImageSizes();
 
-    	return $thumb ? $thumb->url : null;
+        $thumb = $this->getThumb($size, $fallback_sizes);
+
+        return $thumb ? $thumb->url : null;
+    }
+
+    /**
+     * Returns info for the target thumbnail id
+     * 
+     * @param  string $size [description]
+     * @return array        Url, height, width and if the image is cropped
+     */
+    public function getThumbInfo($size)
+    {
+        static::__cacheExistingImageSizes();
+
+        if (!isset($this->sizes[$size])) {
+            
+            $image_data = wp_get_attachment_image_src($this->ID, $size);
+
+            if ($image_data) {
+
+                $this->sizes[$size] = (object) array(
+                    'url'     => $image_data[0],
+                    'width'   => $image_data[1],
+                    'height'  => $image_data[2],
+                    'resized' => $image_data[3],
+                );
+            }
+        }
+
+        return isset($this->sizes[$size]) ? $this->sizes[$size] : null;
+    }
+
+    /**
+     * Used to cache data for all existing image sizes 
+     * 
+     * @return void
+     */
+    public function cacheAllImageSizes()
+    {
+        static::__cacheExistingImageSizes();
+
+        foreach (static::$__sizes as $size) {
+            $this->getThumbInfo($size);
+        }
+    }
+
+    /**
+     * Caches ids of all the existing image sizes registered with WordPress
+     * 
+     * @return void
+     */
+    protected static function __cacheExistingImageSizes()
+    {
+        if (!static::$__sizes)
+            static::$__sizes = get_intermediate_image_sizes();
     }
 }
